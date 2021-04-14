82ecd09a6cb33838bfa70959461bfdaab29750bb
Gzim <gzimasanni@gmail.com>
2021-04-14 17:45:23 +0200

    Add show/forms to the user

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import SearchBar from './SearchBar'
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <SearchBar />
+  </div>;
 };
 
 export default App;
