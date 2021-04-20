d3f3a3d2c4e0d759f84169aef8f792563443057d
Gzim <gzimasanni@gmail.com>
2021-04-20 17:56:12 +0200

    Update readme

--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -15,7 +15,7 @@ class ImageCard extends React.Component {
   setSpans = () => {
     const height = this.imageRef.current.clientHeight;
 
-    const spans = Math.ceil(height / 10)
+    const spans = Math.ceil(height / 10 +1 )
 
     this.setState({ spans })
   } 
