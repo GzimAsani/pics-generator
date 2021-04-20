38e43d44e3ba5d04ee0f387b819c5e074b67e7b5
Gzim <gzimasanni@gmail.com>
2021-04-20 16:08:52 +0200

    Add grid to imageList component

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,11 +1,12 @@
+import './imageList.css'
 import React from 'react'
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-  return <img src={image.urls.regular} />
+  return <img alt={image.description} key={image.id} src={image.urls.regular} />
   })
 
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 }
 
 export default ImageList
